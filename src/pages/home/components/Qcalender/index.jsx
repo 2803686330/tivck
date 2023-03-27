47b2cf268c811f6e51fac2d8e4184d44beeaf460
git@github.com:2803686330/ticket.git <git@github.com:2940063951/test.git>
2023-03-27 21:52:00 +0800

    '获取时间转时间'

--- src/pages/home/components/Qcalender/index.jsx
+++ src/pages/home/components/Qcalender/index.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar } from 'antd-mobile';
 import cs from 'classnames';
 import './styles.less';
 function Qcalender(props) {
   const { className = '', style = {}, setIshow, ishow } = props;
+  const [title, setTitle] = useState();
+  const [time, setTime] = useState(new Date());
+  const [r, setR] = useState();
+  const onChange = (val) => {
+    setIshow(!ishow);
+    const time = val;
+    const year = time.getFullYear();
+    const month = time.getMonth() + 1;
+    const data = time.getDate();
+    const week = time.getDay();
+    const weeks = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+    setTitle(`${year}-${month}-${data}`);
+    setR(`${weeks[week]}`);
+  };
+
+  function getNowFormatDate() {
+    let date = new Date(),
+      year = date.getFullYear(),
+      month = date.getMonth() + 1,
+      strDate = date.getDate();
+    if (month < 10) month = `0${month}`;
+    if (strDate < 10) strDate = `0${strDate}`;
+    return `${year}-${month}-${strDate}`;
+  }
+
+  useEffect(() => {
+    const year = time.getFullYear(); //年
+    const month = time.getMonth() + 1; //月
+    const data = time.getDate(); //日
+    const week = time.getDay(); //星期
+    const weeks = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+    setTitle(`${year}-${month}-${data}`);
+    setR(`${weeks[week]}(今天)`);
+  }, []);
 
   return (
     <Calendar
@@ -14,10 +48,7 @@ function Qcalender(props) {
       prevYearButton={React.ReactNode}
       nextYearButton={React.ReactNode}
       selectionMode="single"
-      onChange={(val) => {
-        console.log(val);
-        setIshow(!ishow);
-      }}
+      onChange={onChange}
     />
   );
 }
